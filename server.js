3c854f10bfdcf67f873bd593d600767a79ac70b4
flotos <flotos@hotmail.fr>
2016-05-13 15:28:36 +0200

    Communication avec back-end en POST

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 //Lets require/import the HTTP module
 var http = require('http');
 var util = require('util');
+var qs = require('querystring');
+var axios = require('axios');
 
 //Lets define a port we want to listen to
 const PORT=8079;
+const API_URL = 'http://localhost:8042/pushWinos';
 
 //We need a function which handles requests and send response
 function handleRequest(request, response){
@@ -13,25 +16,52 @@ function handleRequest(request, response){
 		'Access-Control-Allow-Credentials': 'true'
 	});
 
-    // if(request.url == '/init/'){
+    if(request.url == '/getWinos') {
+        var options = {
+            host: 'localhost',
+            port: 8042,
+            path: '/getWinos'
+        };
+        var callback = function(apiResponse) {
+            var str = '';
+            apiResponse.on('data', function(chunk) {
+                str += chunk;
+            });
+            apiResponse.on('end', function(){
+                response.write(str);
+                response.end('');
+            });
+        }
 
-    var options = {
-        host: 'localhost',
-        port: 8042,
-        path: '/'
-    };
-    var callback = function(apiResponse) {
-        var str = '';
-        apiResponse.on('data', function(chunk) {
-            str += chunk;
+        http.request(options, callback).end();
+    } else if (request.url == '/pushWinos') {
+        // We send receive then follow the data to the API
+
+        // Receive
+        var requestBody = '';
+        request.on('data', function(data) {
+            requestBody += data;
+            if(requestBody.length > 1e7){
+                response.writeHead(413, 'Request Entity Too Large', {'Content-Type': 'text/html'});
+            }
         });
-        apiResponse.on('end', function(){
-            response.write(str);
-            response.end('');
+
+        request.on('end', function() {
+            console.log('data received : '+requestBody);
+
+            // Emit data to the API after reception completes
+            // Should be by TCP protocol after testing
+            axios.post(API_URL,
+                requestBody, {
+              headers: { 
+                "Content-Type": "application/x-www-form-urlencoded"
+              }
+            }).then(function(response) {
+                console.log(response);
+            });
+
         });
     }
-
-    http.request(options, callback).end();
 }
 
 //Create a server
